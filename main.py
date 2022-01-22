f42320014f3ff41f953f3ccdc8dc8c9418ef0411
JK4302 <72574346+JK432@users.noreply.github.com>
2022-01-22 05:44:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Write a python program to print the length of a dictionary
+dic={"name":"India",
+      "capital":"delhi",
+      "PM":"Sir Narendra damodhardas modi",
+      "president":" Sir Ramnath Kovinth",
+      "state":29,
+      "Ourstate":"Kerala"};
+print(len(dic))
